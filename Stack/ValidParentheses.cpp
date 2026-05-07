8ee4820b670a67a6ffdf6aa7a8e7aa36444ffeb5
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-05-07 21:49:35 +0530

    added valid parentheses

---
+++ Stack/ValidParentheses.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c : s) {
+            // opening brackets
+            if(c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            else {
+                // stack empty → invalid
+                if(st.empty()) return false;
+
+                char top = st.top();
+
+                // check matching
+                if((c == ')' && top == '(') ||
+                   (c == '}' && top == '{') ||
+                   (c == ']' && top == '[')) {
+                    st.pop();
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
