2e72273a96572f376c7def32a6e3ace1aee82597
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-05-08 21:01:51 +0530

    fixed the code

--- Stack/ValidParentheses.cpp
+++ Stack/ValidParentheses.cpp
@@ -8,7 +8,24 @@ public:
         for(char c : s) {
             // opening brackets
             if(c == '(' || c == '{' || c == '[') {
-       T
+                st.push(c);
+            }
+            else {
+                // stack empty → invalid
+                if(st.empty()) return false;
+                char top = st.top();
+
+                // check matching
+                if((c == ')' && top == '(') ||
+                   (c == '}' && top == '{') ||
+                   (c == ']' && top == '[')) {
+                    st.pop();
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
 
         return st.empty();
     }
