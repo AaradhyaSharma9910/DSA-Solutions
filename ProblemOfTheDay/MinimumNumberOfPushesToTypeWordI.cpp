40ab5e94702521a535df20f53071c249ce60cfe0
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-30 22:26:27 +0530

    added problem of the day of 30 july

---
+++ ProblemOfTheDay/MinimumNumberOfPushesToTypeWordI.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        int ans = 0;
+        for(int i = 0; i < n; ++i){
+            ans += i / 8 + 1;
+        }
+        return ans;
+    }
+};
